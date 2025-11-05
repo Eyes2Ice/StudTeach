cad5ec355c25011b8d7f802fd5c7e9517eda3fcf
Ruslan <jer_X_2014_projekt@mail.ru>
2025-11-05 13:20:23 +0300

    FAQ section created

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ const swiper = new Swiper(".services__slider", {
   },
 });
 
+// Слайдер отзывов
 new Swiper(".testimonials__slider", {
   spaceBetween: 26,
   slidesPerView: 3,
@@ -17,4 +18,37 @@ new Swiper(".testimonials__slider", {
     nextEl: ".testimonials__next",
     prevEl: ".testimonials__prev",
   },
+});
+
+// Ответы на вопросы
+const accordionLists = document.querySelectorAll(".faq__list");
+
+accordionLists.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    const accordionList = e.currentTarget;
+    const accordionControl = e.target.closest(".faq-item__control");
+    if (!accordionControl) return;
+    e.preventDefault();
+
+    const accordionItem = accordionControl.parentElement;
+    const accordionContent = accordionControl.nextElementSibling;
+    const currentlyOpenedItem =
+      accordionList.querySelector(".faq__item--opened");
+
+    if (accordionItem.classList.contains("faq__item--opened")) {
+      accordionItem.classList.remove("faq__item--opened");
+      accordionContent.style.maxHeight = null;
+      return;
+    }
+
+    if (currentlyOpenedItem && currentlyOpenedItem !== accordionItem) {
+      const openedContent =
+        currentlyOpenedItem.querySelector(".faq-item__content");
+      currentlyOpenedItem.classList.remove("faq__item--opened");
+      openedContent.style.maxHeight = null;
+    }
+
+    accordionItem.classList.add("faq__item--opened");
+    accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+  });
 });
