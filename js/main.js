97430f22fb03eacc5c0859667147f8f593d54a2f
Ruslan <jer_X_2014_projekt@mail.ru>
2025-11-05 12:17:18 +0300

    Testimonials section created

--- js/main.js
+++ js/main.js
@@ -8,3 +8,13 @@ const swiper = new Swiper(".services__slider", {
     prevEl: ".services__prev",
   },
 });
+
+new Swiper(".testimonials__slider", {
+  spaceBetween: 26,
+  slidesPerView: 3,
+
+  navigation: {
+    nextEl: ".testimonials__next",
+    prevEl: ".testimonials__prev",
+  },
+});
