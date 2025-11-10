903264ced261694704240e142fc6203d69a5e6d4
Ruslan <jer_X_2014_projekt@mail.ru>
2025-11-10 12:30:08 +0300

    Testimonials section adapted

--- js/main.js
+++ js/main.js
@@ -34,13 +34,32 @@ const swiper = new Swiper(".services__slider", {
 
 // Слайдер отзывов
 new Swiper(".testimonials__slider", {
-  spaceBetween: 26,
-  slidesPerView: 3,
+  spaceBetween: 20,
+  slidesPerView: 1,
 
   navigation: {
     nextEl: ".testimonials__next",
     prevEl: ".testimonials__prev",
   },
+
+  breakpoints: {
+    1439: {
+      slidesPerView: 3,
+      spaceBetween: 26,
+    },
+    899: {
+      slidesPerView: 2,
+      spaceBetween: 26,
+    },
+  },
+
+  autoplay: {
+    delay: 2000,
+  },
+
+  speed: 600,
+
+  pauseOnInteraction: true,
 });
 
 // Слайдер преимуществ
@@ -61,39 +80,6 @@ new Swiper(".advantages__slider", {
 
   pauseOnInteraction: true,
 });
-
-// Ответы на вопросы
-// const accordionLists = document.querySelectorAll(".faq__list");
-
-// accordionLists.forEach((el) => {
-//   el.addEventListener("click", (e) => {
-//     const accordionList = e.currentTarget;
-//     const accordionControl = e.target.closest(".faq-item__control");
-//     if (!accordionControl) return;
-//     e.preventDefault();
-
-//     const accordionItem = accordionControl.parentElement;
-//     const accordionContent = accordionControl.nextElementSibling;
-//     const currentlyOpenedItem =
-//       accordionList.querySelector(".faq__item--opened");
-
-//     if (accordionItem.classList.contains("faq__item--opened")) {
-//       accordionItem.classList.remove("faq__item--opened");
-//       accordionContent.style.maxHeight = null;
-//       return;
-//     }
-
-//     if (currentlyOpenedItem && currentlyOpenedItem !== accordionItem) {
-//       const openedContent =
-//         currentlyOpenedItem.querySelector(".faq-item__content");
-//       currentlyOpenedItem.classList.remove("faq__item--opened");
-//       openedContent.style.maxHeight = null;
-//     }
-
-//     accordionItem.classList.add("faq__item--opened");
-//     accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
-//   });
-// });
 
 // Ответы на вопросы
 function initAccordion(accordionList) {
